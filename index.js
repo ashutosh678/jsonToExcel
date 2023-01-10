3a2b5b077aad865a481fdc5adaa4e3a7d23233ab
ashutosh678 <102990030+ashutosh678@users.noreply.github.com>
2023-01-10 14:59:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const XLSX = require('xlsx')
+const students = [
+    { name: "Raj", email: "raj@gmail.com", age: 23, gender: "M" },
+    { name: "Rahul", email: "rahul@gmail.com", age: 15, gender: "M" }
+]
+
+const convertJsonToExcel = () => {
+
+    const workSheet = XLSX.utils.json_to_sheet(students);
+    const workBook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(workBook, workSheet, "students")
+    
+    XLSX.write(workBook, { bookType: 'xlsx', type: "buffer" })
+
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
+
+    XLSX.writeFile(workBook, "studentsData.xlsx")
+
+}
+convertJsonToExcel()
